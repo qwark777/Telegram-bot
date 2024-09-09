67fd5ed306a54c8a8adca3908ec0e8692e417a52
qwark777 <159288683+qwark777@users.noreply.github.com>
2024-09-09 22:26:42 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+# import telebot
+
+# bot = telebot.TeleBot("7061086759:AAF_s5oDahOFyjojIVMTGnyU-BEJjxEkgdA")
+# @bot.message_handler(content_types=['text'])
+# def get_text_messages(message):
+#     if message.text == "Привет":
+#         bot.send_message(message.from_user.id, "соси хуй Леша")
+#     elif message.text == "/help":
+#         bot.send_message(message.from_user.id, "Напиши привет")
+#     else:
+#         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+#
+# bot.polling(none_stop=True, interval=0)
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
