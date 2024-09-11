f784e559c39a6590d5b2e367ba06a2ec2c14302b
qwark777 <159288683+qwark777@users.noreply.github.com>
2024-09-11 13:10:39 +0300

    ю

--- registration.py
+++ registration.py
@@ -1,1 +1,14 @@
+import mysql.connector
+from mysql.connector import Error
 
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
