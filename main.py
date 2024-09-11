ca18b7ea68382d746f087940f2b794bb2d0062db
Nadziratel228 <alexalexalex441@yandex.ru>
2024-09-11 21:08:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import Command
 from aiogram.types import Message
 
-router = Router()
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+menu = [
+    [InlineKeyboardButton(text="📝 Генерировать текст", callback_data="generate_text"),
+    InlineKeyboardButton(text="🖼 Генерировать изображение", callback_data="generate_image")],
+    [InlineKeyboardButton(text="💳 Купить токены", callback_data="buy_tokens"),
+    InlineKeyboardButton(text="💰 Баланс", callback_data="balance")],
+    [InlineKeyboardButton(text="💎 Партнёрская программа", callback_data="ref"),
+    InlineKeyboardButton(text="🎁 Бесплатные токены", callback_data="free_tokens")],
+    [InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
+]
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
 
+router = Router()
+bot = Bot(token="7061086759:AAF_s5oDahOFyjojIVMTGnyU-BEJjxEkgdA")
+dp = Dispatcher(storage=MemoryStorage())
 
 async def main():
-    bot = Bot(token="7061086759:AAF_s5oDahOFyjojIVMTGnyU-BEJjxEkgdA")
-
-    dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
     await bot.delete_webhook(drop_pending_updates=True)
