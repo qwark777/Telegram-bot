bb2480db10ad220dd90f775e2aba8969df5dca9d
qwark777 <159288683+qwark777@users.noreply.github.com>
2024-09-11 18:03:40 +0300

    .

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import asyncio
 import logging
-
 from aiogram import Bot, Dispatcher, Router
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import Command
 from aiogram.types import Message
+
 router = Router()
-
-
 
 
 async def main():
@@ -19,13 +17,14 @@ async def main():
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
     await bot.delete_webhook(drop_pending_updates=True)
 
-
 @router.message(Command("start"))
 async def start_handler(msg: Message):
     await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
 @router.message()
 async def message_handler(msg: Message):
     await msg.answer(f"Твой ID: {msg.from_user.id}")
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
+
